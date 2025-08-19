1df70c142aa5c3ca52421c0d2f8aff83875c8ab6
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-08-19 17:16:35 -0300

    while - tabuada

--- Aula3/for.py
+++ Aula3/for.py
@@ -1,4 +1,4 @@
-for i in range(1, 11):
+for i in range(1, 11): # é útil quando sei quantos loopim eu vou fazer.
     # print("2 x", i, "=", 2 * i)  ou
     # print("2 x {} = {}".format(i, 2*i))  ou
     print(f"2 x {i} = {2*i}")
