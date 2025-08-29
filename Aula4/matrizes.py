addc680137ec57d059bf2e501ec74d0d329b4213
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-08-29 19:41:40 -0300

    Matrizes II

---
+++ Aula4/matrizes.py
@@ -0,0 +1,11 @@
+numeros = [
+    [3, 4, 7, 8],
+    [1, 3, 6, 2],
+    [6, 9, 7, 82],
+    [7, 5, 0, 81],
+]
+
+#for i in numeros:
+ #   print(i)
+
+print(numeros[2][1]) # Acessando o elemento na linha 2, coluna 1 (índices começam em 0)
