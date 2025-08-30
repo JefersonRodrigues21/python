6391a34215722a397baa9831b4724697c128635c
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-08-29 21:55:21 -0300

    Aula4 Coleções

---
+++ Aula4/colecoes.py
@@ -0,0 +1,18 @@
+# conjuntos
+conjunto = set([4, 7, 2, 3, 0, 8])
+print('Conjunto:', conjunto)
+
+# tupla
+tupla = (3, 2, 4, 6, 0)
+print(tupla)
+
+pessoa = {'nome': 'Jeferson', 'telefone': '(65)98999-9999', 'endereco': 'ABC'}
+print(pessoa['telefone'])
+
+pessoas = [
+    {'nome': 'Jeferson', 'telefone': '(65) 99999-9999', 'endereco': 'ABC'},
+    {'nome': 'Maria', 'telefone': '(65) 98888-8888', 'endereco': 'DEF'},
+    {'nome': 'João', 'telefone': '(65) 97777-7777', 'endereco': 'GHI'}
+]
+
+print(pessoas[2]['telefone'])  # Acessando o nome da segunda pessoa na lista
