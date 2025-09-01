e3c3aaacba2dce3de59cdf8cd4cc2390d498e576
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-09-01 15:38:17 -0300

    Aula6 funções em python

---
+++ Aula6/funcoes.py
@@ -0,0 +1,19 @@
+
+def dividir(n1, n2):
+    if n2 == 0:
+        print("Divisão por zero não é permitida.")
+    else:
+        resultado = n1 / n2
+        # print(f"{n1} / {n2} = {resultado}")
+        return resultado
+
+divisao = dividir(80, 0)
+print('O resultado da divisão é:', divisao)
+
+print('Resultado', dividir(20, 4))
+
+resultado = dividir(3, 1)
+soma = 20 + resultado
+print('A soma é:', soma)
+
+dividir(6, 0)
