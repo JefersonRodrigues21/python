27675e95c4fdf526d3e8b26468148b880bcdf5e4
JefersonRodrigues21 <dev.geekjs@gmail.com>
2025-09-03 15:52:12 -0300

    Aula 8 - Conceitos Básicos da Orientação a Objetos III

--- Aula8/Produto.py
+++ Aula8/Produto.py
@@ -1,16 +1,34 @@
 class Produto:
 
-    def __init__(self, nome, valor, marca = '', modelo = ''):
+    def __init__(self, nome, valor, quantidade = 0, marca = '', modelo = ''):
         self.nome = nome
         self.valor = valor
+        self.quantidade = quantidade
         self.marca = marca
         self.modelo = modelo
+    
+    def vender(self, quantidade):
+        if(quantidade > self.quantidade):
+            print('*******************************')
+            print('Quantidade indisponível')
+            print('Quantidade máxima:', self.quantidade)
+            print('*******************************')
+        else:
+            self.quantidade -= quantidade
+    
+    def comprar(self, quantidade):
+        self.quantidade += quantidade
 
-produto0 = Produto('Celular', 1500, 'Samsung', 'Galaxy S21')
 
-produto1 = Produto('Geladeira', 2500, 'Brastemp', 'BRM44')
+produto0 = Produto('Celular', 2000, 100, 'Samsung', 'Galaxy S21')
+produto0.comprar(10)
+produto0.vender(111)
 
-produto2 = Produto('Notebook', 5000, 'Dell', 'Inspiron 15')
+produto1 = Produto('Geladeira', 4000, 50, 'Brastemp', 'BRM44')
+
+produto2 = Produto('Notebook', 4000)
+produto2.comprar(100)
+produto2.vender(1)
 
 
 print(produto0.__dict__)
